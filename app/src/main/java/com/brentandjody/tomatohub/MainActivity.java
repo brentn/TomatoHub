7e9e6e59a32323fa5db1d3ce433a5d6fdb093915
brentn <nesbitt.brent@gmail.com>
2015-11-30 06:54:53 -0800

    Make more robust

--- app/src/main/java/com/brentandjody/tomatohub/MainActivity.java
+++ app/src/main/java/com/brentandjody/tomatohub/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,22 +25,24 @@ import android.widget.TextView;
 import com.jcraft.jsch.*;
 
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements OverviewFragment.OnFragmentInteractionListener {
+
     public static final String routerIPPref = "prefRouterIP";
     public static final  String routerPort = "prefRouterPort";
     public static final  String routerUserPref = "prefRouterUser";
     public static final String routerPasswordPref = "prefRouterPass";
 
     private SharedPreferences mPrefs;
+    private Session mSession;
     private String mIpAddress;
     private int mPort;
     private String mUser;
     private String mPassword;
+    private boolean mStartActivityHasBeenRun=false;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -92,9 +95,18 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSession != null) {
+            mSession.disconnect();
+            mSession = null;
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        new updateStats().execute();
+        new SSHLogon().execute();
     }
 
     @Override
@@ -102,76 +114,6 @@ public class MainActivity extends AppCompatActivity
         //TODO:
     }
 
-    private class updateStats extends AsyncTask<Void,Void,Void>
-    {
-        String mWanInterface;
-        String[] mLanInterfaces;
-        String[] mLanClients;
-        @Override
-        protected Void doInBackground(Void... voids) {
-            JSch ssh = new JSch();
-            try {
-                java.util.Properties config = new java.util.Properties();
-                config.put("StrictHostKeyChecking", "no");
-                Session session = ssh.getSession(mUser, mIpAddress, mPort);
-                session.setConfig(config);
-                session.setPassword(mPassword);
-                session.connect(5000);
-                mWanInterface = runBySSH("nvram show|grep wan_iface|cut -d= -f2", session)[0];
-                mLanInterfaces = runBySSH("arp|cut -d' ' -f8|sort -u|grep -v "+mWanInterface, session);
-                mLanClients = new String[mLanInterfaces.length];
-                for (int i=0;i<mLanInterfaces.length; i++ ) {
-                    mLanClients[i] = runBySSH("arp|grep "+mLanInterfaces[i]+"|wc -l", session)[0];
-                }
-                session.disconnect();
-            } catch(Exception ex) {
-                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            int total=0;
-            for (int i=0;i<5;i++){
-                int id=-1;
-                switch (i){
-                    case 0:id=R.id.lan_0;break;
-                    case 1:id=R.id.lan_1;break;
-                    case 2:id=R.id.lan_2;break;
-                    case 3:id=R.id.lan_3;break;
-                    case 4:id=R.id.lan_4;break;
-                }
-                TextView view = (TextView)mViewPager.findViewById(id);
-                if (i<mLanClients.length) {
-                    try {total += Integer.parseInt(mLanClients[i]);}
-                    catch(Exception ex) {}
-                    view.setVisibility(View.VISIBLE);
-                    view.setText(mLanClients[i]);
-                } else {
-                    view.setVisibility(View.GONE);
-                }
-            }
-            ((TextView)mViewPager.findViewById(R.id.devices)).setText(String.valueOf(total)+" devices");
-        }
-
-    }
-
-    private String[] runBySSH(String command, Session session) throws Exception {
-        Channel channel=session.openChannel("exec");
-        ((ChannelExec)channel).setCommand(command);
-        ByteArrayOutputStream sb=new ByteArrayOutputStream();
-        channel.setOutputStream(sb);
-        channel.connect();
-        while (!channel.isClosed()) {
-            Thread.sleep(10);
-        }
-        channel.disconnect();
-        List<String> lines = Arrays.asList(sb.toString().split("\n"));
-        lines.removeAll(Arrays.asList("", null));
-        return lines.toArray(new String[lines.size()]);
-    }
 
 
     @Override
@@ -195,6 +137,21 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setStatusMessage(String message) {
+        TextView view = (TextView)mViewPager.findViewById(R.id.status_message);
+        view.setText(message);
+        view.setVisibility(message.isEmpty()?View.GONE:View.VISIBLE);
+    }
+
+    private void setDevicesMessage(String numDevices, String deviceStatus) {
+        TextView devices = (TextView)mViewPager.findViewById(R.id.devices);
+        TextView areconnected = (TextView)mViewPager.findViewById(R.id.are_connected);
+        devices.setText(numDevices);
+        devices.setVisibility(numDevices.isEmpty()?View.GONE:View.VISIBLE);
+        areconnected.setText(deviceStatus);
+        areconnected.setVisibility(deviceStatus.isEmpty()?View.GONE:View.VISIBLE);
     }
 
     /**
@@ -229,4 +186,112 @@ public class MainActivity extends AppCompatActivity
             return null;
         }
     }
+
+    private class SSHLogon extends AsyncTask<Void,Void,Void>
+    {
+        boolean success = false;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            JSch ssh = new JSch();
+            try {
+                java.util.Properties config = new java.util.Properties();
+                config.put("StrictHostKeyChecking", "no");
+                mSession = ssh.getSession(mUser, mIpAddress, mPort);
+                mSession.setConfig(config);
+                mSession.setPassword(mPassword);
+                mSession.connect(5000);
+                success = true;
+            } catch (Exception ex) {
+                if (mSession!=null) {
+                    mSession.disconnect();
+                    mSession = null;
+                }
+                if (!mStartActivityHasBeenRun) {
+                    mStartActivityHasBeenRun = true;
+                    startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (success) {
+                setStatusMessage("Everything looks good.");
+                new ValueInitializer().execute();
+            } else {
+                setStatusMessage("Could not connect to router.");
+            }
+        }
+    }
+
+    private class ValueInitializer extends AsyncTask<Void, Void, Void> {
+        String mWanInterface;
+        String[] mLanInterfaces;
+        String[] mLanClients;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                mWanInterface = sshCommand("nvram show|grep wan_iface|cut -d= -f2")[0];
+                mLanInterfaces = sshCommand("arp|cut -d' ' -f8|sort -u|grep -v " + mWanInterface);
+                mLanClients = new String[mLanInterfaces.length];
+                for (int i = 0; i < mLanInterfaces.length; i++) {
+                    mLanClients[i] = sshCommand("arp|grep " + mLanInterfaces[i] + "|wc -l")[0];
+                }
+            } catch(Exception ex) {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            int total=0;
+            for (int i=0;i<5;i++){
+                int id=-1;
+                switch (i){
+                    case 0:id=R.id.lan_0;break;
+                    case 1:id=R.id.lan_1;break;
+                    case 2:id=R.id.lan_2;break;
+                    case 3:id=R.id.lan_3;break;
+                    case 4:id=R.id.lan_4;break;
+                }
+                TextView view = (TextView)mViewPager.findViewById(id);
+                if (i<mLanClients.length) {
+                    try {total += Integer.parseInt(mLanClients[i]);}
+                    catch(Exception ex) {}
+                    view.setVisibility(View.VISIBLE);
+                    view.setText(mLanClients[i]);
+                } else {
+                    view.setVisibility(View.GONE);
+                }
+            }
+            setDevicesMessage(String.valueOf(total)+ " devices", " are connected.");
+        }
+    }
+
+    private String[] sshCommand(String command) throws Exception {
+        if (mSession!=null) {
+            try {
+                Channel channel = mSession.openChannel("exec");
+                ((ChannelExec) channel).setCommand(command);
+                ByteArrayOutputStream sb = new ByteArrayOutputStream();
+                channel.setOutputStream(sb);
+                channel.connect();
+                while (!channel.isClosed()) {
+                    Thread.sleep(10);
+                }
+                channel.disconnect();
+                List<String> lines = Arrays.asList(sb.toString().split("\n"));
+                lines.removeAll(Arrays.asList("", null));
+                return lines.toArray(new String[lines.size()]);
+            } catch (Exception ex) {
+                Log.e("TomatoHub", ex.getMessage()+ex.getStackTrace());
+            }
+        }
+        return new String[0];
+    }
+
 }
